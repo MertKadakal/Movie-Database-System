9d229f56852b298fb0aaca4abfed1c61205b4cb6
MertKadakal <mert.kadakal1629@gmail.com>
2024-08-08 23:58:47 +0300

    People.txt dosyasından veri alımı switch-case yapıldı

--- src/ArtistPerformerChildActor.java
+++ src/ArtistPerformerChildActor.java
@@ -1,7 +1,16 @@
 public class ArtistPerformerChildActor extends ArtistPerformer {
+    private String age;
 
-    public ArtistPerformerChildActor(String id, String name, String surname, String country) {
+    public ArtistPerformerChildActor(String id, String name, String surname, String country, String age) {
         super(id, name, surname, country);
+        this.age = age;
     }
-    
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
 }
--- src/MovieDatabaseSys.java
+++ src/MovieDatabaseSys.java
@@ -1,12 +1,37 @@
+import java.util.ArrayList;
+
 public class MovieDatabaseSys {
+
     public static void main(String[] args) {
         String[] commands_txt = FileInput.readFile("src\\IO_1\\commands.txt", true, true);
         String[] films_txt = FileInput.readFile("src\\IO_1\\films.txt", true, true);
         String[] people_txt = FileInput.readFile("src\\IO_1\\people.txt", true, true);
         String output_path = "src\\output.txt";
 
-        for (int i =0; i < films_txt.length; i++) {
-            System.out.println();
+        ArrayList<Person> people = new ArrayList<>();
+        //obtain people datas
+        for (int i =0; i < people_txt.length; i++) {
+            String[] person = people_txt[i].split("\t");
+            switch (person[0]) {
+                case ("Director"):
+                    people.add(new ArtistDirector(person[1], person[2], person[3], person[4], person[5]));
+                    break;
+                case ("Writer"):
+                    people.add(new ArtistWriter(person[1], person[2], person[3], person[4], person[5]));
+                    break;
+                case ("Actor"):
+                    people.add(new ArtistPerformerActor(person[1], person[2], person[3], person[4], person[5]));
+                    break;
+                case ("ChildActor"):
+                    people.add(new ArtistPerformerChildActor(person[1], person[2], person[3], person[4], person[5]));
+                    break;
+                case ("StuntPerformer"):
+                    people.add(new ArtistPerformerStuntPerformer(person[1], person[2], person[3], person[4], person[5]));
+                    break;
+                case ("User"):
+                    people.add(new User(person[1], person[2], person[3], person[4]));
+                    break;
+            }
         }
     }
 }
